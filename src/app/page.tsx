e629dd9b4e4ce1570c87c14bc95011e0e4e52207
Nathan Rothenberger <nroth10@gmail.com>
2024-06-19 12:53:00 -0500

    feat: wip black box styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,14 +23,17 @@ export default function HomePage() {
       <section className='bg-bg'>
         {/* <ViewStarter /> */}
         <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          {/* Black Box */}
-          <div className='bg-fg text-bg px-6 py-9'>
+          {/*
+           * Black Box
+           * Clamp here instead of on text line lengths for mobile
+           */}
+          <div className='bg-fg text-bg px-6 py-9 flex flex-col gap-9 max-w-[456px] sm:flex-row sm:items-start sm:max-w-[9000px] sm:bg-green-300 md:bg-slate-300 lg:bg-black'>
             {/* Magic box */}
-            <div className='border-4 border-dotted border-bg  aspect-square min-w-12 max-w-[168px] w-[20vw]' />
+            <div className='border-4 border-dotted border-bg  aspect-square min-w-12 max-w-[128px] w-[20vw]' />
             {/* Text */}
-            <div className='flex flex-col gap-10 items-start text-left max-w-[460px]'>
+            <div className='flex flex-col gap-10 items-start text-left max-w-[460px] sm:max-w-[90000px]'>
               <div className='flex flex-col gap-0 items-start '>
-                <h2 className='text-2xl font-normal leading-tight bg-red-300'>
+                <h2 className='text-2xl font-bold leading-tight'>
                   How to build a button
                 </h2>
                 <p className='text-2xl font-normal italic leading-tight '>
@@ -38,7 +41,7 @@ export default function HomePage() {
                 </p>
               </div>
 
-              <p className='text-2xl font-normal italic leading-tight bg-red-200'>
+              <p className='text-2xl font-normal leading-tight'>
                 Learn how to design and develop a button, deliver components for
                 your team, and manage abstraction.
               </p>
