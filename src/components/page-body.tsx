7242a0c1a34b5824864727b400512584d823fefe
Nathan Rothenberger <nroth10@gmail.com>
2024-06-22 11:34:47 -0500

    fix: more tweaking

--- src/components/page-body.tsx
+++ src/components/page-body.tsx
@@ -11,7 +11,8 @@ export const PageBody = ({ children, className }: PagebodyProps) => {
     <section
       className={cn(
         'layout relative flex min-h-screen flex-col items-center justify-start pt-0 pb-12',
-        'sm:justify-start sm:px-4 sm:py-4',
+        'md:justify-center sm:px-4 sm:py-4',
+        // 'sm:bg-red-400 md:bg-green-400',
         className
       )}
     >
